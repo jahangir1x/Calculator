91b51e54684ac235e8b81b2ea601de934cf58359
Md. Jahangir Alam <70907172+x00jahangir@users.noreply.github.com>
2022-01-07 23:30:09 +0600

    UI improvements
    # polished About window
    # added application logo

---
+++ out/production/Calculator/src/io/github/x00jahangir/OperationType.java
@@ -0,0 +1,14 @@
+package io.github.x00jahangir;
+
+// supported operation types
+public enum OperationType {
+    ADD,
+    SUBTRACT,
+    MULTIPLY,
+    DIVIDE,
+    SQUARE_ROOT,
+    SQUARE,
+    INVERT,
+    NEGATE,
+    NONE
+}
